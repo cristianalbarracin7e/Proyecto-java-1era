fd4671856d7f5d68142404f7277e4ae78af56ace
cristianalbarracin7e <cristianalbarracin7e@gmail.com>
2026-08-18 19:41:56 -0500

    subiendo app y mi clase

---
+++ src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    
+    public static void main(String[] args) throws Exception {
+        int a =5;
+        int b =10; 
+        System.out.println("Hello, World!");
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+    }
+}
---
+++ src/MiClase.java
@@ -0,0 +1,22 @@
+public class MiClase{
+    private int edad = 28;
+    private  double altura = 1.87;
+    private char sexo = 'm';
+    private boolean aprobado = false;
+    private String nombre = "Cristian Albarracin";
+    private String saludo = "Hola," + nombre;
+    private double nota;
+    private char nota = '90';
+    private double precio = 9.99;
+    private preciocortado (int)precio ;
+
+
+
+    
+
+
+
+
+
+
+}
